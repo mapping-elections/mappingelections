d3ffeeb22280b9c4bfe45dd9e41222e9328857fe
Jordan Bratt <jfbratt@gmail.com>
2017-07-26 19:08:13 -0400

    Update get_related_elections function and add documentation

--- R/map-yaml.R
+++ R/map-yaml.R
@@ -1,10 +1,18 @@
 #' Generate the YAML for each Congressional election map
 #'
-#' @param meae_id A unique Mapping Early American Elections id
+#' @param meae_id A unique Mapping Early American Elections id.
+#' @param legend_type A description of the data visualization. Default set to fed-vs-reub-percentage.
+#' @param always_allow_html Default to TRUE.
+#' @param layout Type of html layout. Default set to maps-show.
+#'
+#' @return A list of all the needed metadata for rendering the maps in Jekyll
 #'
 #' @export
 generate_map_yaml <- function(meae_id, legend_type = "fed-vs-repub-percentage",
                               always_allow_html = "true", layout = "maps-show"){
+
+   ## stopifnot( as.character(meae_id),
+   ## length(strsplit(meae_id, "[.]")[[1]]) == 5)
 
     md_doc <- list(variant = "markdown")
     html_doc <- list(theme = "default")
@@ -18,7 +26,7 @@ generate_map_yaml <- function(meae_id, legend_type = "fed-vs-repub-percentage",
     connected_maps <- get_related_maps(meae_id)
 
 
-    output <- c(basic_info, caption_title, legend_type, related_elections, connected_maps, final_docs)
+    output <- c(basic_info, caption_title, related_elections, connected_maps, final_docs)
     output
 }
 
@@ -36,7 +44,7 @@ get_general_info <- function(meae_id){
 get_title_caption <- function(basic_info){
   ordinal <- as.character(congress_numbering[congress_numbering$number == basic_info$congressnum, 2])
   elect_year <- meae_maps_to_elections[meae_maps_to_elections$meae_id == basic_info$meae_id, 2]
-  election_year <- as.character(meae_elections[meae_elections$election_id == as.character(elect_year[1]), 6])
+  election_year <- as.character(meae_elections[meae_elections$election_id == as.character(elect_year[[1]][1]), 6])
 
   map_title <- paste0("Elections for ", proper_case(ordinal), " Congress in ",
                      proper_case(basic_info$state), ", ", election_year)
@@ -54,17 +62,24 @@ proper_case <- function(x){
 # Need to order the districts numerically
 get_related_elections <- function(meae_id){
 
+  rel_elect <- meae_maps_to_elections[meae_maps_to_elections$meae_id == meae_id, 2]
   election_list <- list()
-  rel_elect <- meae_maps_to_elections[meae_maps_to_elections$meae_id==meae_id, 2]
 
+  if(nrow(rel_elect) == 1){
+    election_info <- meae_elections[meae_elections$election_id == rel_elect[[1]][1], ]
+    name_desc <- paste0(election_info$state, " US Congress ", election_info$year, " At Large")
+    election <- list(id = rel_elect[[1]][i], name = name_desc)
+    election_list <- election
+  }else {
   for(i in 1:nrow(rel_elect)){
 
-    election_info <- meae_elections[meae_elections$election_id == rel_elect[[1]][i], ]
     name_desc <- paste0(election_info$state, " US Congress ", election_info$year, " District ", election_info$district)
+    election_info <- meae_elections[meae_elections$election_id == rel_elect[[1]][i], ]
 
     election <- list(id = rel_elect[[1]][i], name = name_desc)
     election_list <-  c(election_list, list(election))
 
+  }
 }
 
 related_elections <- list(nnv = election_list)
@@ -81,17 +96,18 @@ get_related_maps <- function(meae_id){
 
   maps_list <- c(list(prev_map), list(nex_map), list(nat_map))
 
-  related_maps_list <- list(related-maps = maps_list)
+  related_maps_list <- list(`related-maps` = maps_list)
 }
 
 previous_map <- function(mapping_id, congress_num){
 
   if(congress_num >= 2){
     if(congress_num <= 9){
-      previous_congress <- paste0("0", as.character(congress_num + 1))
+      previous_congress <- paste0("0", as.character(congress_num - 1))
     }else {
-      previous_congress <- as.character(congress_num + 1)
+      previous_congress <- as.character(congress_num - 1)
     }
+    ## Spacing ISSUE
 
     previous_id <- paste0(mapping_id[[1]][1], ".", mapping_id[[1]][2], ".", "congress", previous_congress, ".",
                       mapping_id[[1]][4], ".", mapping_id[[1]][5])
