66edc37b93628c2e497a444eb60fa024d0094f98
Lincoln Mullen <lincoln@lincolnmullen.com>
2017-07-31 17:02:15 -0400

    Move ordinal numbering table to internal data

--- data-raw/congress_numbering.R
+++ data-raw/congress_numbering.R
@@ -2,4 +2,5 @@ library(tidyverse)
 
 congress_numbering <- read_csv("data-raw/congress_numbering.csv")
 
-devtools::use_data(congress_numbering, overwrite = TRUE, compress = "xz")
+devtools::use_data(congress_numbering, internal = TRUE, overwrite = TRUE,
+                   compress = "xz")
